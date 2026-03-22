671e689401f0c7c5ac1924478bece3d46011e3b5
Jackson Crim <jacksoncrim@gmail.com>
2026-03-22 19:49:36 -0400

    changes to Array.hpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include "LinkedList.hpp"
 #include "Array.hpp"
 int main() {
+  std::cout << "Hello\n";
+  Array<int> myarr = Array();
+  myarr->push_back(10);
+  printf("%d", myarr);
 
+
+  return 0;
 }
