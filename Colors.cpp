b2a1bf549f16e73d034a256c8a2c4754b8cf80e4
Jackson Crim <jacksoncrim@ymail.com>
2026-03-28 21:58:39 -0400

    Removed Colors.cpp (unused)

--- Colors.cpp
+++
@@ -1,9 +0,0 @@
-#include "Colors.hpp"
-
-std::string red(const std::string &text) {
-  return "\033[91m" + text + "\033[0m";
-}
-
-std::string green(const std::string &text) {
-  return "\033[92m" + text + "\033[0m";
-}
